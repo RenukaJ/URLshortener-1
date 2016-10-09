79b6e8d83367c09b191345377d2062883ee1c254
Renuka Joshi <renu.joshi4@gmail.com>
2016-10-09 17:00:50 -0500

    Refactored project to URLShortner
    Tested in local. working. Deleting the older customer support.

---
+++ URLShortner/source/production/java/com/cpsc476/urlshortner/URLShortner.java
@@ -0,0 +1,11 @@
+package com.cpsc476.urlshortner;
+import org.apache.commons.codec.binary.Base64;
+public class URLShortner {
+// convert url to shortened url
+	public URLShortner(String url) {
+		
+	}
+	
+	
+	
+}

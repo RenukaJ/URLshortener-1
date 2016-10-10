a500afb38014895bbc65ed024bd7225921e92f08
RenukaJ <RenukaJ@192.168.0.16>
2016-10-09 17:50:55 -0700

    Adding accessors and mutators for URLShortner

--- URLShortner/source/production/java/com/cpsc476/urlshortner/URLShortner.java
+++ URLShortner/source/production/java/com/cpsc476/urlshortner/URLShortner.java
@@ -2,8 +2,15 @@ package com.cpsc476.urlshortner;
 import org.apache.commons.codec.binary.Base64;
 public class URLShortner {
 // convert url to shortened url
+	String shortUrl = "";
 	public URLShortner(String url) {
-		
+	
+	}
+	public String getShortUrl() {
+		return shortUrl;
+	}
+	public void setShortUrl(String shortUrl) {
+		this.shortUrl = shortUrl;
 	}
 	
 	

335f5fa8f8c3ffe4dbdb5a251b44f8e62607ae76
NeenuAVarghese <neenuavarghese@gmail.com>
2016-10-26 15:51:25 -0700

    Added Comments, Formatted Files. Final Commit for CPSC476-Project 1

---
+++ URLShortner/target/classes/controller/UrlRequesthandler.java
@@ -0,0 +1,155 @@
+package controller;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.DBRequesthandler;
+
+/*
+ * This servlet handles requests and responses for all responses for the domainname/short/* URL
+ */
+@WebServlet(
+		name = "shortURLHandler",
+		urlPatterns = "/short/*"
+		)
+
+public class UrlRequesthandler extends HttpServlet{
+
+	DBRequesthandler reqHandler = new DBRequesthandler();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException , IOException
+	{
+		/*This part handles the actions which can happen on the home page*/
+		String action = request.getParameter("action");
+
+		if(action == null)
+			action = "page";
+
+		switch(action)
+		{
+		case "gotoUrl":
+			this.gotoUrl(request, response);
+			break;
+		case "page":
+		default:
+			browserUrlRequstAction(request, response);
+			break;
+		}
+	}
+
+	/*Handle requests from User Click from User profile page*/
+	public void gotoUrl(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException
+	{
+		/*
+		 * 1. Get URL from request parameters
+		 * 2. Check if the short URL exists in DB
+		 * 		2.a YES
+		 * 			1. Get the corresponding long url
+		 * 			2. Add the count for URL vists
+		 * 			3. Try to make a coonection the the Long/original URL
+		 * 			4. Get response code
+		 * 			5. Check if Response Code is 22
+		 * 				YES:
+		 * 				a. Redirect to the Original URL
+		 * 				No:
+		 * 				a. Redirect to Error Page
+		 * 		2.b No
+		 * 			Redirect to Error Page
+		 */
+
+		String orgUrl = "";
+		String shUrl = request.getParameter("url");
+
+		if(reqHandler.shortUrlexists(shUrl)){
+			orgUrl = reqHandler.getLongUrl(shUrl);
+			reqHandler.addUrlVisitCount(shUrl);
+
+			int responseCode = 404;
+
+			try{
+				HttpURLConnection huc = (HttpURLConnection) new URL(orgUrl).openConnection();
+				responseCode = huc.getResponseCode();
+			}
+			catch(Exception e){
+				responseCode = 404;
+			}
+			if (responseCode == 200) {
+
+				response.sendRedirect(orgUrl);
+			}
+			else{
+				response.sendRedirect("/URLShortner/errorPage");
+			}
+		}
+		else{
+			response.sendRedirect("/URLShortner/errorPage");
+			return;
+		}
+
+	}
+
+
+	/*Handle requests from User query from browser*/
+	public void browserUrlRequstAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException
+	{
+
+		/*
+		 * 1. Get URL from request parameters
+		 * 2. Append http://localhost:8080 to the URL
+		 * 3. Check if the short URL exists in DB
+		 * 		3.a YES
+		 * 			1. Get the corresponding long url
+		 * 			2. Add the count for URL vists
+		 * 			3. Try to make a coonection the the Long/original URL
+		 * 			4. Get response code
+		 * 			5. Check if Response Code is 22
+		 * 				YES:
+		 * 				a. Redirect to the Original URL
+		 * 				No:
+		 * 				a. Redirect to Error Page
+		 * 		3.b No
+		 * 			Redirect to Error Page
+		 */
+		String shUrl = request.getRequestURI();
+		shUrl = "http://localhost:8080" + shUrl;
+
+		String orgUrl = "";
+
+		if(reqHandler.shortUrlexists(shUrl)){
+			orgUrl = reqHandler.getLongUrl(shUrl);
+			reqHandler.addUrlVisitCount(shUrl);
+
+			int responseCode = 404;
+			try{
+				HttpURLConnection huc = (HttpURLConnection) new URL(orgUrl).openConnection();
+				responseCode = huc.getResponseCode();
+			}
+			catch(Exception e){
+				responseCode = 404;
+			}
+
+			if (responseCode == 200) {
+				response.sendRedirect(orgUrl);
+			}
+			else{
+				response.sendRedirect("/URLShortner/errorPage");
+			}
+
+		}
+		else{
+			response.sendRedirect("/URLShortner/errorPage");
+			return;
+		}
+	}
+}
+
+
